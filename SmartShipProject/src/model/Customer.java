1bd32315ff694d4996846ce3cafb9d2c17690b38
CHEVANNESE MARIE ELLIS <158519006+Chevannese@users.noreply.github.com>
2025-10-31 18:52:57 -0500

    Add files via upload

---
+++ SmartShipProject/src/model/Customer.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Customer extends User
+{
+	public Customer()
+	{
+		super();
+	}
+	
+	public Customer(String trn, String firstName, String lastName, String password, String contactNum, String email)
+	{
+		super(trn, firstName, lastName, password, contactNum, email);
+	}
+	
+	public Customer(Customer cust)
+	{
+		super(cust);
+	}
+	
+	
+}
