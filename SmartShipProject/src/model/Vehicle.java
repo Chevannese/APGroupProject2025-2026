55f5d6443021007d8904179f01bf998373b25cbe
CHEVANNESE MARIE ELLIS <158519006+Chevannese@users.noreply.github.com>
2025-11-02 18:52:34 -0500

    Create Vehicle.java

---
+++ SmartShipProject/src/model/Vehicle.java
@@ -0,0 +1,77 @@
+package model;
+
+public class Vehicle 
+{
+	private String vehicleNo;
+	private String vehicleName;
+	private int quantityCap;
+	private double weightCap;
+	
+	
+	public Vehicle()
+	{
+		vehicleNo = "";
+		vehicleName = "";
+		quantityCap = 0;
+		weightCap = 0;
+	}
+
+
+	public Vehicle(String vehicleNo, String vehicleName, int quantityCap, double weightCap) {
+		this.vehicleNo = vehicleNo;
+		this.vehicleName = vehicleName;
+		this.quantityCap = quantityCap;
+		this.weightCap = weightCap;
+	}
+	
+	public Vehicle(Vehicle vehicle) {
+		this.vehicleNo = vehicle.vehicleNo;
+		this.vehicleName = vehicle.vehicleName;
+		this.quantityCap = vehicle.quantityCap;
+		this.weightCap = vehicle.weightCap;
+	}
+
+
+	public String getVehicleNo() {
+		return vehicleNo;
+	}
+
+
+	public void setVehicleNo(String vehicleNo) {
+		this.vehicleNo = vehicleNo;
+	}
+
+
+	public String getVehicleName() {
+		return vehicleName;
+	}
+
+
+	public void setVehicleName(String vehicleName) {
+		this.vehicleName = vehicleName;
+	}
+
+
+	public int getQuantityCap() {
+		return quantityCap;
+	}
+
+
+	public void setQuantityCap(int quantityCap) {
+		this.quantityCap = quantityCap;
+	}
+
+
+	public double getWeightCap() {
+		return weightCap;
+	}
+
+
+	public void setWeightCap(double weightCap) {
+		this.weightCap = weightCap;
+	}
+	
+	
+	
+	
+}
