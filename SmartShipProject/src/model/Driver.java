31a2ee46844ada024069f37581fe5c471a6f8364
CHEVANNESE MARIE ELLIS <158519006+Chevannese@users.noreply.github.com>
2025-11-03 09:27:48 -0500

    Update Driver.java

--- SmartShipProject/src/model/Driver.java
+++ SmartShipProject/src/model/Driver.java
@@ -7,32 +7,15 @@ public class Driver extends User
 		super();
 	}
 	
-	public Driver(String trn, String firstName, String lastName, String password, String contactNum, String email)
+	public Driver(String trn, String firstName, String lastName, String password, String contactNum, String email, String userType)
 	{
-		super(trn, firstName, lastName, password,contactNum, email);
+		super(trn, firstName, lastName, password,contactNum, email, userType);
 	}
 	
 	public Driver(Driver driver)
 	{
 		super(driver);
 	}
-
-    public List<String> getPackages() {
-       
-        return List.of("PKG001 - In Transit", "PKG002 - Delivered");
-    }
-
-    public boolean updatePackageStatus(String trackingNumber, String newStatus) {
-        System.out.println("Updating " + trackingNumber + " to " + newStatus);
-        return true;
-    }
-
-    public String getRoute() {
-        return "Zone 3 - Kingston East";
-    }
-
-    public String getVehicleInfo() {
-        return "Vehicle: Toyota Hiace (Reg #V-102)";
-    }
+	
 	
 }
