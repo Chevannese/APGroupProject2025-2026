b55ea7c0684f05665c3298f84d560708264a8572
CHEVANNESE MARIE ELLIS <158519006+Chevannese@users.noreply.github.com>
2025-11-03 09:28:16 -0500

    Update Clerk.java

--- SmartShipProject/src/model/Clerk.java
+++ SmartShipProject/src/model/Clerk.java
@@ -7,9 +7,9 @@ public class Clerk extends User
 		super();
 	}
 	
-	public Clerk(String trn, String firstName, String lastName, String password, String contactNum, String email)
+	public Clerk(String trn, String firstName, String lastName, String password, String contactNum, String email, String userType)
 	{
-		super(trn, firstName, lastName, password, contactNum, email);
+		super(trn, firstName, lastName, password, contactNum, email, userType);
 	}
 	
 	public Clerk(Clerk clerk)
