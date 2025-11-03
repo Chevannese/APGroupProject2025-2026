11807a87777e22753e15dcca963bd8259a3960bc
CHEVANNESE MARIE ELLIS <158519006+Chevannese@users.noreply.github.com>
2025-11-03 00:11:20 -0500

    Create MainWindow.java

---
+++ SmartShipProject/src/view/MainWindow.java
@@ -0,0 +1,257 @@
+package view;
+
+import java.awt.CardLayout;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+
+import model.User;
+
+
+
+
+public class MainWindow extends JFrame 
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private boolean exception = false;
+	private CardLayout cardLayout, loginCardLayout;
+    private JPanel mainPanel, loginPanel, customerPanel, driverPanel, clerkPanel, managerPanel;
+	private GridBagConstraints gc;
+	private User loggedInUser = new User();
+
+
+    
+    
+    private void initializeSectionPanels()
+    {
+    	setTitle("Login System");
+        setSize(400, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        
+		
+    	cardLayout = new CardLayout();
+    	loginCardLayout = new CardLayout();
+    	
+		mainPanel = new JPanel(cardLayout);
+		
+		loginPanel = new JPanel(cardLayout);
+
+		customerPanel = new JPanel(cardLayout);
+		driverPanel = new JPanel(cardLayout);
+		clerkPanel = new JPanel(cardLayout);
+		managerPanel = new JPanel(cardLayout);
+    }
+    
+    private void initializeLoginComponents()
+    {
+    	 // Create the two cards
+        JPanel signInPanel = new JPanel(new GridBagLayout());
+        JPanel signUpPanel = new JPanel(new GridBagLayout());
+
+        loginPanel.add(signInPanel, "Login");
+        loginPanel.add(signUpPanel, "Register");
+        mainPanel.add(loginPanel, "LoginPage");
+
+        add(mainPanel);
+        cardLayout.show(mainPanel, "LoginPage"); // start with sign-in
+
+        gc = new GridBagConstraints();
+        gc.insets = new Insets(5, 5, 5, 5);
+        gc.fill = GridBagConstraints.HORIZONTAL;
+
+        JLabel title = new JLabel("Sign In", SwingConstants.CENTER);
+        title.setFont(new Font("Arial", Font.BOLD, 20));
+
+        final JTextField usernameFieldL = new JTextField(15);
+        final JPasswordField passwordFieldL = new JPasswordField(15);
+        JButton signInBtn = new JButton("Sign In");
+        JButton goToSignUpBtn = new JButton("Create Account");
+
+        addToGridBag(signInPanel, title, gc, 0, 0, 2, 1);
+        addToGridBag(signInPanel, new JLabel("Username:"), gc, 0, 1, 1, 1);
+        addToGridBag(signInPanel, usernameFieldL, gc, 1, 1, 1, 1);
+
+
+        addToGridBag(signInPanel, new JLabel("Password:"),gc, 0, 2, 1, 1);
+        addToGridBag(signInPanel, passwordFieldL, gc, 1, 2, 1, 1);
+
+        addToGridBag(signInPanel, signInBtn, gc, 0, 3, 2, 1);
+        addToGridBag(signInPanel, goToSignUpBtn,gc, 0, 4, 2, 1);
+
+        goToSignUpBtn.addActionListener(e -> cardLayout.show(loginPanel, "Register"));
+
+        signInBtn.addActionListener(e -> {
+            String user = usernameFieldL.getText();
+            String pass = new String(passwordFieldL.getPassword());
+            // Dummy check
+            if (user.equals("admin") && pass.equals("1234")) {
+                JOptionPane.showMessageDialog(this, "Welcome " + user + "!");
+            } else {
+                JOptionPane.showMessageDialog(this, "Invalid username or password.");
+            }
+        });
+
+        // sign up
+        gc.insets = new Insets(5, 5, 5, 5);
+        gc.fill = GridBagConstraints.HORIZONTAL;
+
+        title = new JLabel("Sign Up", SwingConstants.CENTER);
+        title.setFont(new Font("Arial", Font.BOLD, 20));
+
+        final JTextField trnField = new JTextField(9);
+        final JPasswordField passwordFieldR = new JPasswordField(15);
+        JTextField firstNameField = new JTextField(20);
+        JTextField lastNameField = new JTextField(20);
+        JTextField contactNumField = new JTextField(20);
+        JTextField emailField = new JTextField(20);
+        
+        JButton signUpBtn = new JButton("Register");
+        JButton goToSignInBtn = new JButton("Back to Sign In");
+
+        addToGridBag(signUpPanel, title, gc, 0, 0, 2, 1);
+        
+        addToGridBag(signUpPanel, new JLabel("First Name:"), gc, 0, 1, 1, 1);
+        addToGridBag(signUpPanel, firstNameField, gc, 1, 1, 1, 1);
+
+        addToGridBag(signUpPanel, new JLabel("Last Name:"),gc, 0, 2, 1, 1);
+        addToGridBag(signUpPanel, lastNameField,gc, 1, 2, 1, 1);
+        
+        addToGridBag(signUpPanel, new JLabel("TRN:"),gc, 0, 3, 1, 1);
+        addToGridBag(signUpPanel, trnField,gc, 1, 3, 1, 1);
+
+        addToGridBag(signUpPanel, new JLabel("Contact Number: "),gc, 0, 4, 1, 1);
+        addToGridBag(signUpPanel, contactNumField,gc, 1, 4, 1, 1);
+
+        addToGridBag(signUpPanel, new JLabel("Email: "),gc, 0, 5, 1, 1);
+        addToGridBag(signUpPanel, emailField,gc, 1, 5, 1, 1);
+        
+        addToGridBag(signUpPanel, new JLabel("Password:"),gc, 0, 6, 1, 1);
+        addToGridBag(signUpPanel, passwordFieldR,gc, 1, 6, 1, 1);
+        
+        addToGridBag(signUpPanel, signUpBtn,gc, 0, 7, 2, 1);
+
+        addToGridBag(signUpPanel, goToSignInBtn,gc, 0, 8, 2, 1);
+
+        goToSignInBtn.addActionListener(e -> cardLayout.show(loginPanel, "Login"));
+
+        signUpBtn.addActionListener(e -> {
+        	exception = false;
+        	if(firstNameField.getText().compareTo("") == 0 || lastNameField.getText().compareTo("") == 0 ||
+        		trnField.getText().compareTo("") == 0 || contactNumField.getText().compareTo("") == 0 ||
+        		emailField.getText().compareTo("") == 0)
+	        {
+	        	JOptionPane.showMessageDialog( 
+		        		SwingUtilities.getWindowAncestor(mainPanel),
+		        		 "One or two fields were not filled");
+	        }
+	        else
+	        {
+	        	String firstName = firstNameField.getText();
+	        	String lastName = lastNameField.getText();
+	        	String trn = trnField.getText();
+	        	String contactNum = contactNumField.getText();
+	        	String email = emailField.getText();
+	        	if(trn.length() != trnField.getColumns())
+				{
+					JOptionPane.showMessageDialog( 
+			        		this,
+			        		 "TRN must be exactly 9 digits in the format: 123456789");
+					return;
+				}
+	        	else
+					
+					try
+					{
+						//Temporary assigns temp to the converted number of TextField from receiverTRNTxt
+						//to check that all values are number
+						int temp = Integer.parseInt(trnField.getText());
+
+					}catch(NumberFormatException nf)
+					{
+						JOptionPane.showMessageDialog(SwingUtilities.getWindowAncestor(mainPanel),"Invalid Format for TRN field\n"
+								+ "Must be in format: 123456789");
+						exception = true;
+					}
+	        		
+
+					if(exception == false)
+					{
+
+
+						JOptionPane.showMessageDialog(this, "Account created for " + firstName + " " + lastName + "!");
+			            cardLayout.show(loginPanel, "Login");
+					}
+	        }
+            
+        });
+
+    }
+    
+    
+    
+    private void initializeCustomerPanelComponents()
+    {
+    	
+    	
+    }
+    
+    public void addToGridBag(JPanel panel, Component component, GridBagConstraints gbc, int column, int row,  int colspan, int rowspan)
+	{
+
+		if (panel == null ) {
+		        System.err.println("Panel is null!");
+		        return;
+		    }
+		
+		if(component == null)
+		{
+			System.err.println("Component is null!");
+	        return;
+		}
+			
+	
+	
+	        gbc.gridx = column;
+	        gbc.gridy = row;
+	
+	        gbc.gridwidth = colspan;
+	        gbc.gridheight = rowspan;
+	        
+	        
+	        panel.add(component,gbc);
+    }
+    
+    
+    public MainWindow()
+    {
+    	initializeSectionPanels();
+    	initializeLoginComponents();
+		setVisible(true);
+
+    	
+    }
+    public static void main(String[] args)
+    {
+    	new MainWindow();
+    }
+    
+
+}
